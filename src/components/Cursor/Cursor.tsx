ec6bd94803768c0d27d38420ed62e62973811c5e
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-31 07:25:25 +0100

    completed the custom cursor app! under 2hrs... thank you GODgit add .!

--- src/components/Cursor/Cursor.tsx
+++ src/components/Cursor/Cursor.tsx
@@ -1,14 +1,16 @@
-import {useEffect, useCallback} from 'react'
+import {useEffect, useCallback, useRef} from 'react'
 import { motion, useMotionValue, useSpring } from 'framer-motion'
 import './Cursor.scss'
 
 
 export default function Cursor() {
+    const cursorRef = useRef<HTMLDivElement>({} as HTMLDivElement)
     const defX = useMotionValue<number>(-50)
     const defY = useMotionValue<number>(-50)
     const transition = {duration:0.1, bounce: 0}
     const x = useSpring(defX, transition)
     const y = useSpring(defY, transition)
+    const scale = useMotionValue<number>(1)
 
 
     const updateMousePosition = useCallback((event: MouseEvent) => {
@@ -19,11 +21,15 @@ export default function Cursor() {
         defX.set(mousePositionX);
         defY.set(mousePositionY);
 
-        if (targetElement.classList.contains('.ch1')) {
-            console.log('we have a target')
+        // console.log(event.target, targetElement.classList)
+        if (targetElement.classList.contains('ch1')) {
+            scale.set(10)
+            cursorRef.current.classList.add('inAction')
+        } else {
+            scale.set(1)
+            cursorRef.current.classList.remove('inAction')
         }
-    }, [defX, defY],
-    )
+    }, [defX, defY, scale])
     
 
     // adds the event listener that will update the x and y position of the custom cursor
@@ -37,6 +43,6 @@ export default function Cursor() {
     
 
     return (
-        <motion.div className="cursor" style={{x, y}}></motion.div>
+        <motion.div className="cursor" ref={cursorRef} style={{x, y, scale}}></motion.div>
     )
 }
