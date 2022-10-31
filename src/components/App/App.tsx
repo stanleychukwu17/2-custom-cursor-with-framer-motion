2573e1d926c998a196780fad4fde06d14ff00a66
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-31 08:05:59 +0100

    minor update final commit, thank you GODgit add .!

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,34 @@
+import {motion} from 'framer-motion'
 import Cursor from '../Cursor/Cursor';
+
 import './app.scss';
+
+const introVariant = {
+    initial: {
+    },
+    animate: {
+        opacity: [0, .5, 1],
+        y: [60, -30, 0],
+        transition: {
+            y: {
+                times: [0, .9, 1],
+                duration: 1,
+            },
+            opacity: {
+                times: [0, .9, 1],
+                duration: 1,
+            },
+            duration: 1
+        }
+    }
+}
 
 const App = () => {
     return (
         <div className="AppMain">
-            <div className="mainText">
-                <h1 className='ch1'>Hello World</h1>
-            </div>
+            <motion.div className="mainText" variants={introVariant} initial="initial" animate="animate">
+                <h1 className='ch1'>Hi. STANLEY</h1>
+            </motion.div>
             <Cursor />
         </div>
     )
