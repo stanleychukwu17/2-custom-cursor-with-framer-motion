856d63851d5d234f42ce0198270ca65d1e590484
stanley chukwu <stanleychukwu17@yahoo.com>
2022-10-31 05:45:02 +0100

    started working on the custom cursor following the user mouse

--- src/components/Cursor/Cursor.tsx
+++ src/components/Cursor/Cursor.tsx
@@ -1,8 +1,26 @@
+import {useEffect, useCallback} from 'react'
+import { motion, useMotionValue } from 'framer-motion'
 import './Cursor.scss'
 
 
 export default function Cursor() {
+    const x = useMotionValue<number>(-50);
+    const y = useMotionValue<number>(-50)
+
+    const updateMousePosition = useCallback(() => {
+        const mousePosition = 0
+    }, [],
+    )
+    
+
+    useEffect(() => {
+
+        return () => {
+        }
+    }, [])
+    
+
     return (
-        <div className="cursor"></div>
+        <motion.div className="cursor" style={{x, y}}></motion.div>
     )
 }
